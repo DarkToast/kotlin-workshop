16be91f5867f4ae4997842de64f56d64be0eec50
Christian Iwanzik <c.iwanzik@tarent.de>
2018-05-04 09:44:07 +0200

    Adding a new Main class and adding the assembly plugin

---
+++ examples/src/main/kotlin/session1/Main.kt
@@ -0,0 +1,18 @@
+package session1
+
+fun main(args: Array<String>): Unit {
+    println("Hello World")
+}
+
+
+/*
+kotlin.*
+kotlin.annotation.*
+kotlin.collections.*
+kotlin.comparisons.* (since 1.1)
+kotlin.io.*
+kotlin.ranges.*
+kotlin.sequences.*
+kotlin.text.*
+
+*/
