a02dd1f20f9b064c487ea8a64621db9de46c831a
Christian Iwanzik <c.iwanzik@tarent.de>
2018-06-08 12:56:25 +0200

    Adding examples and description for delegations and extensions.

---
+++ examples/src/main/kotlin/session4/Delegation.kt
@@ -0,0 +1,45 @@
+package session4
+
+/**
+ * Delegations bieten in Kotlin syntactic sugar für delegate objects, sodass man ein direktes Durchschleifen
+ * elegant abkürzen kann und einem den Grundsatz "Composition over Inheritance" wesentlich komfortabler zu gestalten.
+ *
+ * Ein Interface, welches die Methoden `printMessage` und `printMessageLine` definiert.
+ */
+interface Printer {
+    fun printMessage(message: String)
+    fun printMessageLine(message: String)
+}
+
+/**
+ * Diese Implementierung schreibt alle Eingaben einfach auf Stdout. Mal mit einem Newline, mal ohne.
+ */
+class StdOutPrinter: Printer {
+    override fun printMessage(value: String) = print(value)
+    override fun printMessageLine(value: String) = println(value)
+}
+
+/**
+ * Nun definieren wir uns eine Delegierungsfassade.
+ * Anstatt von StdOutPrinter abzuleiten, verwenden wir hier die Delegierung.
+ * - Per Konstruktorargument erhalten wir zunächst das Zielobjekt der Delegierung. Einen `Printer`.
+ * - Wir "implementieren" daraufhin das Interface, geben aber per `by` unser Delegierungsobjekt aus dem Konstruktor an.
+ *   Kotlin wird nun `PrinterFacade` zum `Printer` machen und alle nicht selbst implementierten Methoden des Interfaces
+ *   automatisch an unser Zielobjekt delegieren.
+ * - Syntaktisch sieht es nun fast wie eine Ableitung aus. Wie sind nun aber wesentlich flexibler da wir selbst
+ *   entscheiden können, zu welchem Zielobjekt wir delegieren können.
+ */
+class PrinterFacade(printer: Printer): Printer by printer {
+    override fun printMessageLine(message: String) = println("Hello $message")
+}
+
+/**
+ * Wir komponieren uns nun einen Printer, bestehend aus einer `PrinterFacade` und einem `StdOutPrinter` als `Delegierungsobjekt.
+ * - Der Aufruf von `printMessage` wird nun die Implementierung von `StdOutPrinter` nutzen.
+ * - Der Aufruf von `printMessageLine` wird die Implementierung von `PrinterFacade` nutzen.
+ */
+fun main(args: Array<String>) {
+    val printer = PrinterFacade(StdOutPrinter())
+    printer.printMessage("Hallo Welt")
+    printer.printMessageLine("Hallo Welt")
+}
---
+++ examples/src/main/kotlin/session4/Extensions.kt
@@ -0,0 +1,43 @@
+package session4
+
+import session1.JavaContract
+
+/**
+ * Kotlin erlaubt es, bestehende Klassen um neue Methoden zu erweitern. Diese "Extensions" sind dabei nur dort
+ * gültig, in dessen Scope sie auch definiert wurden. Die Beispiele hier, sind im globalen Scope definiert und daher
+ * auch von überall aus erreichbar.
+ *
+ * Als Beispiel ziehen wir unsere Java Klasse aus section1 zur Rate. Diese Klasse ist final und kann nicht abgeleitet werden.
+ * Mit Kotlin "Extensions" können wir aber trotzdem eine neue Methode hinzufügen.
+ * Dafür definieren wir folgende Methode:
+ *  - Als Name geben wir die Zielklasse und die gewünschte neue Methode an.
+ *  - Der Rest, also Rückgabetyp und Parametertypen bleibt gleich
+ *  - Innerhalb der Methode existiert eine `this`-Referenz, die auf das entsprechende Objekt des erweiterten Typs
+ *    zeigt.
+ *
+ * "Extensions" können nur auf die `public`-Elemente des Zieltyps zugreifen. Intern handelt es sich dabei lediglich
+ * um einen Wrappertyp, der die neuen Methoden beinhaltet. Der Compiler bietet es uns lediglich als Teil der Zielklasse an.
+ */
+fun JavaContract.getBaseFeePerYear(): Double {
+    return this.getBaseFee() * 12;
+}
+
+fun JavaContract.getReducedYearBaseFee(): Double {
+    return this.getBaseFeePerYear() * 0.95;
+}
+
+fun main(args: Array<String>) {
+
+    // Wenn wir nun ein konkretes JavaContract Objekt besitzen...
+    val contract = JavaContract()
+
+    // ... so können wir neben den eigentlichen Methoden auch die erweiteren Methoden benutzen.
+    println(contract.baseFee)
+    println(contract.getBaseFeePerYear())
+    println(contract.getReducedYearBaseFee())
+}
+
+
+
+
+
