982f8e748d214376aabb2e5808c90e2c467848df
Christian Iwanzik <c.iwanzik@tarent.de>
2018-07-09 11:43:23 +0200

    Using another annotation.

--- spring-kotlin/src/main/kotlin/spring/hello/HelloController.kt
+++ spring-kotlin/src/main/kotlin/spring/hello/HelloController.kt
@@ -2,13 +2,14 @@ package spring.hello
 
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.GET
 
 @Controller
 class HelloController(private val helloService: HelloService) {
 
-    @RequestMapping("/hello", method = [GET])
+    @GetMapping("/hello")
     fun helloKotlin(): ResponseEntity<String> {
         return ResponseEntity.ok(helloService.getHello())
     }
