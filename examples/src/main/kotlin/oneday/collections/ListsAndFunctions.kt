9a99852eee32bf4fae296b9ea3ae394402409d69
Christian Iwanzik <c.iwanzik@tarent.de>
2019-04-09 13:05:51 +0200

    Adding examples to list and functions

---
+++ examples/src/main/kotlin/oneday/collections/ListsAndFunctions.kt
@@ -0,0 +1,40 @@
+package oneday.collections
+
+import java.util.*
+
+fun functions() {
+
+    // Kotlin bringt seine eigene Colletions-API mit.
+    // Die Sprache unterscheidet zwischen immutable Collections, wie hier der `List`
+    val list: List<String> = listOf("blue", "green", "red")
+
+    // und mutable Lists. Eine neue Liste kann per "Hand" erzeugt werden,
+    // Kotlin bietet aber auch globale Helfermethoden an.
+    val mutableList: MutableList<String> = ArrayList()  // = mutableListOf("blue", "green", "red")
+    mutableList.add("blue")
+    mutableList.add("green")
+    mutableList.add("red")
+
+    // Danaben gibt auch viele andere Arten
+    val map: Map<String, String> = mapOf(Pair("foo", "FOO"), Pair("bar", "BAR"))
+    val mutableMap: Map<String, String> = mutableMapOf(Pair("foo", "FOO"), Pair("bar", "BAR"))
+    val set: Set<String> = setOf("blue", "blue", "green", "red")
+
+    // Die Collections haben, wie z.B. auch bei Java 8 Streams, monadische Methoden, welche einen Lambdaausdruck
+    // entgegennehmen. Hier als Beispiel ein Filter:
+    val blueList = list.filter({ value: String -> value == "blue" })
+
+    // Die runden Klammern kann man auch weg lassen:
+    val upperCaseList = list.map { value -> value.toUpperCase() }
+
+    // Lambdas kann man in Kotlin auch direkt als Funktionen einer Variable zuweisen.
+    // Der Typ der FUnktion setzt sich dabei aus den Eingabe- und Ausgabetypen zusammen:
+    val toUpperCase: (String) -> String = { value -> value.toUpperCase() }
+    list.map(toUpperCase)
+
+
+    // Die Lambdaausdrücke und Funktionen sind auch mit dem Java 8 SDK kompatibel:
+    val optional = Optional.of("Foobar")
+    val upperCase = optional.map { value -> value.toUpperCase()}
+    val upperCase2 = optional.map(toUpperCase)
+}
