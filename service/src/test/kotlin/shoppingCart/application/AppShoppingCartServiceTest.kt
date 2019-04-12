b373856e83e82aea07fd17bbda35a9dcb8c4120d
Christian Iwanzik <c.iwanzik@tarent.de>
2019-04-12 16:35:32 +0200

    Adding the service test

--- service/src/test/kotlin/shoppingCart/application/AppShoppingCartServiceTest.kt
+++ service/src/test/kotlin/shoppingCart/application/AppShoppingCartServiceTest.kt
@@ -3,24 +3,13 @@ package shoppingCart.application
 import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.core.test.TestCase
 import org.mockito.Mockito
+import org.mockito.Mockito.times
 import shoppingCart.domain.*
 import shoppingCart.ports.driven.database.ShoppingCartRepositoryPort
 import shoppingCart.ports.driven.productService.ProductRepositoryPort
 import java.util.*
 
-/*
- * Aufgabe: 6
- * Entfernen Sie bitte die Kommentare von den hier drunter liegenden Tests und implementieren Sie die Methoden
- * in einer eigens von Ihnen angelegten oder erweiteren Klasse.
- *
- * Bitte implementieren Sie nach und nach die einzelnen Teste der Klasse shoppingCart.application.AppShoppingCartService.
- *
- * Einige Teste haben eine Beschreibung, die mit einem '!' beginnen. Diese sind zur Zeit inaktiv. Entfernen Sie
- * bitte das Ausrufezeichen, um den Test zu aktivieren und ihn implementieren zu können.
- *
- * Das Ziel ist, mit Containerformaten wie Collectiond uns Option-Werten zu arbeiten.
-*/
-class ServiceTest: FeatureSpec() {
+class AppShoppingCartServiceTest: FeatureSpec() {
     private var shoppingCartPort = Mockito.mock(ShoppingCartRepositoryPort::class.java)
     private val productPort = Mockito.mock(ProductRepositoryPort::class.java)
     private val sku = SKU("123456")
@@ -35,8 +24,8 @@ class ServiceTest: FeatureSpec() {
 
     init {
         feature("The shopping cart service") {
-/*            scenario("if not existing, the service returns empty") {
-                val service: ShoppingCartService = AppShoppingCartService(shoppingCartPort)
+            scenario("if not existing, the service returns empty") {
+                val service = AppShoppingCartService(shoppingCartPort, productPort)
 
                 Mockito.`when`(shoppingCartPort.load(uuid)).thenReturn(Optional.empty())
 
@@ -44,8 +33,8 @@ class ServiceTest: FeatureSpec() {
                 result.isPresent shouldBe false
             }
 
-            scenario("!if exists the shopping cart has the product") {
-                val service: ShoppingCartService = AppShoppingCartService(shoppingCartPort)
+            scenario("if exists the shopping cart has the product") {
+                val service = AppShoppingCartService(shoppingCartPort, productPort)
 
                 Mockito.`when`(shoppingCartPort.load(uuid)).thenReturn(Optional.of(shoppingCart))
                 Mockito.`when`(productPort.findProductBySku(sku)).thenReturn(Optional.of(milk))
@@ -58,8 +47,8 @@ class ServiceTest: FeatureSpec() {
                 result.get().content() shouldContain Pair(milk, Quantity(2))
             }
 
-            scenario("!A not existing product throws an exception") {
-                val service: ShoppingCartService = AppShoppingCartService(shoppingCartPort)
+            scenario("if exists the shopping cart has no product on a unknown SKU") {
+                val service = AppShoppingCartService(shoppingCartPort, productPort)
 
                 Mockito.`when`(shoppingCartPort.load(uuid)).thenReturn(Optional.of(shoppingCart))
                 Mockito.`when`(productPort.findProductBySku(sku)).thenReturn(Optional.empty())
@@ -67,8 +56,8 @@ class ServiceTest: FeatureSpec() {
                 shouldThrow<ProductNotFoundException> { service.putProductIntoShoppingCart(uuid, sku, Quantity(2)) }
             }
 
-            scenario("!if exists the shopping cart should be saved") {
-                val service: ShoppingCartService = AppShoppingCartService(shoppingCartPort)
+            scenario("if exists the shopping cart should be saved") {
+                val service = AppShoppingCartService(shoppingCartPort, productPort)
 
                 Mockito.`when`(shoppingCartPort.load(uuid)).thenReturn(Optional.of(shoppingCart))
                 Mockito.`when`(productPort.findProductBySku(sku)).thenReturn(Optional.of(milk))
@@ -76,7 +65,7 @@ class ServiceTest: FeatureSpec() {
                 service.putProductIntoShoppingCart(uuid, sku, Quantity(2))
 
                 Mockito.verify(shoppingCartPort, times(1)).save(shoppingCart)
-            }*/
+            }
         }
     }
 }
