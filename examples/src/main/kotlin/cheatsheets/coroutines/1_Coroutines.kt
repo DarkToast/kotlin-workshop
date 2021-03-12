279b65d40604064d7901f1ecb6ae8f85f2224265
Christian Schmidt <c.schmidt@tarent.de>
2021-03-12 16:21:19 +0100

    More context knowhow

--- examples/src/main/kotlin/cheatsheets/coroutines/1_Coroutines.kt
+++ examples/src/main/kotlin/cheatsheets/coroutines/1_Coroutines.kt
@@ -2,13 +2,14 @@ package cheatsheets.coroutines
 
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.yield
+import kotlinx.coroutines.withContext
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -34,7 +35,7 @@ fun main() {
     val deferred: Deferred<Int> = GlobalScope.async {
         delay(1000)
         println("Async")
-        13 + 29
+        13 + 28
     }
 
     // Auf das Ergebnis kann mit `await` gewartet werden.
@@ -47,50 +48,74 @@ fun main() {
 
     println(result)
 
-    // Ein Beispiel mit verschachtelten Funktionen:
-    // Da wir im Main-Thread sind, erzeugen wir mit `runBlocking` einen neuen Context um die Threads zu
-    // synchronisieren.
     runBlocking {
-        // Wir benutzen `measureTimeMillis` um die gemeinsame Laufzeit zu messen.
-        // Wir rufen intern die beiden Methoden hintereinander auf:
-        val time = measureTimeMillis {
-            // Obwohl wir suspending Functions aufrufen, starten wir sie aber nicht in ihrer eigenen Coroutine.
-            // Ein automatischer asynchroner Start funtioniert also nicht.
-            createIntOne()
-            createIntTwo()
+        // Ein Beispiel mit verschachtelten Funktionen:
+        // Da wir im Main-Thread sind, erzeugen wir mit `runBlocking` einen neuen Context um die Threads zu
+        // synchronisieren.
+        val job1 = GlobalScope.launch {
+            // Wir benutzen `measureTimeMillis` um die gemeinsame Laufzeit zu messen.
+            // Wir rufen intern die beiden Methoden hintereinander auf:
+            val time = measureTimeMillis {
+                // Obwohl wir suspending Functions aufrufen, starten wir sie aber nicht in ihrer eigenen Coroutine.
+                // Ein automatischer asynchroner Start funtioniert also nicht.
+                createIntOne()
+                createIntTwo()
+            }
+
+            // Die Gesamtlaufzeit ist bei ~2s
+            println("Completed in $time ms")
         }
+        job1.join()
 
-        // Die Gesamtlaufzeit ist bei ~2s
-        println("Completed in $time ms")
-    }
 
-    // Um wirklich die susending Functions asynchron zu starten, rufen wir sie in einem `async`-Block auf:
-    runBlocking {
-        val time = measureTimeMillis {
-            // btw: Coroutinen lassen sich mit `start` ein wenig in ihrer Ausführung steuern.
-            // Hier: Mit Lazy wird die Coroutine erst beim ersten Ergebniszugriff gestartet.
-            val one = async(start = CoroutineStart.LAZY) { createIntOne() }
-            val two = async { createIntTwo() }
 
-            println("The answer is ${one.await() + two.await()}")
+        // Um wirklich die susending Functions asynchron zu starten, rufen wir sie in einem `async`-Block auf:
+        val job2 = GlobalScope.launch {
+            val time = measureTimeMillis {
+                // btw: Coroutinen lassen sich mit `start` ein wenig in ihrer Ausführung steuern.
+                // Hier: Mit Lazy wird die Coroutine erst beim ersten Ergebniszugriff gestartet.
+                val one = async(start = CoroutineStart.LAZY) { createIntOne() }
+                val two = async { createIntTwo() }
+
+                println("The answer is ${one.await() + two.await()}")
+            }
+
+            // Die Gesamtlaufzeit ist bei ~1s -> Asynchron!
+            println("Completed in $time ms")
         }
-
-        // Die Gesamtlaufzeit ist bei ~1s -> Asynchron!
-        println("Completed in $time ms")
+        job2.join()
     }
 }
 
-// Suspending Functions können direkt in Coroutinen als asynchrone Funktion aufgerufen werden
-// und haben intrinsisch den Coroutinenscope mit dabei. Wir können hier also direkt andere
-// suspending Functione wie z.B. `delay` aufrufen.
+// Suspending Functions können direkt in Coroutinen als Funktion aufgerufen werden
+// und haben intrinsisch den Coroutinenscope mit dabei. Sie werden vom Compiler so umgebaut,
+// dass sie von den Coroutinen jederzeit gestoppt und wieder angefahren werden können, ohne den darunter
+// liegenden Thread zu blockieren.
+// Wir können hier also direkt andere suspending Functione wie z.B. `delay` aufrufen.
 suspend fun createIntOne(): Int {
-    println("ONE")
+    println("ONE - thread: ${Thread.currentThread().name}")
     delay(1000L) // not blocking
     return 13
 }
 
+// Achtung! Ruft eine `suspend function` eine "normale" Funktion auf,
+// die wiederum einen blockierenden Call macht, so blockiert auch hier der
+// Thread der Coroutine.
+//
+// Wie geht man nun mit blockierenden IO Operationen um?
+// Hier bietet die API den expliziten IO Dispatcher, der blockierendes IO
+// auf eigens gestartete IO-Threads auslagert:
 suspend fun createIntTwo(): Int {
-    println("TWO")
-    delay(1000L) // pretend we are doing something useful here, too
+    println("TWO - thread: ${Thread.currentThread().name}")
+    delay(500L) // pretend we are doing something useful here, too
+
+    return withContext(Dispatchers.IO) {
+        println("IO - thread: ${Thread.currentThread().name}")
+        ioFunction()
+    }
+}
+
+fun ioFunction(): Int {
+    Thread.sleep(500)
     return 29
 }
