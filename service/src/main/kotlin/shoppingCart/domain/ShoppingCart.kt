76ff95df3382ad2acfdc68dbdd7b574a9efa8303
Christian Schmidt <c.schmidt@tarent.de>
2022-06-01 12:38:28 +0200

    Implementing ShoppingCart

--- service/src/main/kotlin/shoppingCart/domain/ShoppingCart.kt
+++ service/src/main/kotlin/shoppingCart/domain/ShoppingCart.kt
@@ -17,20 +17,72 @@ data class Item(val product: Product, val quantity: Quantity) {
 
 class ShoppingCart(
     val shoppingCartUuid: ShoppingCartUuid = ShoppingCartUuid(),
-    private val items: List<Item> = emptyList()
+    items: List<Item> = emptyList()
 ) {
+    private var cartItems: Map<SKU, Item> = items.associateBy { it.product.sku }
+        set(value) {
+            amount = value.amount()
+            field = value
+        }
 
-    fun items() = items
+    private var amount: Amount = cartItems.amount()
 
-    fun amount(): Amount = Amount(0, 0)
+    private fun Map<SKU, Item>.amount() = this.values.fold(Amount(0, 0)) { amount, item ->
+        amount + item.amount
+    }
 
-    fun isEmpty(): Boolean = false
+    fun amount(): Amount = amount
 
-    fun addProduct(product: Product, quantity: Quantity): ShoppingCart = this
+    fun isEmpty(): Boolean = cartItems.isEmpty()
 
-    fun quantityOfProduct(sku: SKU): Optional<Quantity> = Optional.empty()
+    fun addProduct(product: Product, quantity: Quantity): ShoppingCart {
+        checkMaximumProductCount()
 
-    fun content(): List<Pair<Product, Quantity>> = listOf()
+        val mutatedItems = cartItems.toMutableMap()
+
+        val item = Optional.ofNullable(mutatedItems[product.sku])
+            .map { it.addQuantity(quantity) }
+            .orElseGet { Item(product, product.price, quantity) }
+
+        mutatedItems[product.sku] = item
+        cartItems = mutatedItems
+        return this
+    }
+
+    fun quantityOfProduct(sku: SKU): Optional<Quantity> {
+        return Optional.ofNullable(cartItems[sku]).map { it.quantity }
+    }
+
+    fun items(): List<Item> {
+        return cartItems.values.toList()
+    }
+
+    private fun checkMaximumProductCount() {
+        if (cartItems.count() >= 50) {
+            throw MaximumProductCountExceededException(cartItems.count())
+        }
+    }
+
+
+    // -- Equals - HashCode --
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ShoppingCart
+
+        if (shoppingCartUuid != other.shoppingCartUuid) return false
+        if (cartItems != other.cartItems) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = shoppingCartUuid.hashCode()
+        result = 31 * result + cartItems.hashCode()
+        return result
+    }
 }
 
 data class ShoppingCartUuid(val uuid: UUID = UUID.randomUUID()) {
