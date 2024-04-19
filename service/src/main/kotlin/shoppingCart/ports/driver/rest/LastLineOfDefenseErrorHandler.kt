6516361ae40101c13d64f8bed43a6c3f417cf0cc
Christian Schmidt <c.schmidt@qvest-digital.com>
2024-04-19 13:02:37 +0200

    Adding the shoppingCart error implementation

--- service/src/main/kotlin/shoppingCart/ports/driver/rest/LastLineOfDefenseErrorHandler.kt
+++ service/src/main/kotlin/shoppingCart/ports/driver/rest/LastLineOfDefenseErrorHandler.kt
@@ -5,10 +5,12 @@ import jakarta.servlet.http.HttpServletRequest
 import mu.KotlinLogging
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import shoppingCart.application.ApplicationException
+import shoppingCart.application.ProductNotFoundException
 import shoppingCart.domain.DomainException
 import shoppingCart.ports.PortException
 import shoppingCart.ports.driver.rest.dto.Failure
@@ -17,8 +19,12 @@ import java.time.LocalDateTime
 @ControllerAdvice
 class LastLineOfDefenseErrorHandler {
 
+    private val logger = KotlinLogging.logger {}
+
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleBadRequest(ex: IllegalArgumentException, request: HttpServletRequest): ResponseEntity<Failure> {
+        logger.warn(ex) { "For request with illegal arguments. Returning ${BAD_REQUEST.name}" }
+
         val now = LocalDateTime.now().toString()
         val message = ex.message ?: ""
         val error = Failure(now, BAD_REQUEST.value(), BAD_REQUEST.reasonPhrase, message, request.requestURI)
@@ -28,6 +34,18 @@ class LastLineOfDefenseErrorHandler {
 
     @ExceptionHandler(value = [DomainException::class, ApplicationException::class, PortException::class])
     fun handleDomainException(ex: RuntimeException, request: HttpServletRequest): ResponseEntity<Failure> {
-        return ResponseEntity.badRequest().build()
+        val httpStatus = when (ex) {
+            is ProductNotFoundException -> NOT_FOUND
+            is ShoppingCartNotFoundException -> NOT_FOUND
+            else -> BAD_REQUEST
+        }
+
+        logger.warn(ex) { "Got domain explicit failure. Returning ${httpStatus.name}" }
+
+        val now = LocalDateTime.now().toString()
+        val message = ex.message ?: ""
+        val error = Failure(now, httpStatus.value(), httpStatus.reasonPhrase, message, request.requestURI)
+
+        return ResponseEntity(error, HttpHeaders(), httpStatus)
     }
 }
